6ba1e3a915311c8304ee236eb782699e114ee6c6
srisarvesh <r.srisarvesh@gmail.com>
2021-08-20 10:24:27 +0530

    Updates

---
+++ precache-manifest.7823cdb4a885661e46038e3b6004ca97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8ec754f540d1d3f5b5",
+    "url": "/JU_DISCUSSION/static/js/main.8e8ec754.chunk.js"
+  },
+  {
+    "revision": "33d36acc7869fcda5d8e",
+    "url": "/JU_DISCUSSION/static/js/1.33d36acc.chunk.js"
+  },
+  {
+    "revision": "459d726709078f11a09b",
+    "url": "/JU_DISCUSSION/static/js/runtime~main.459d7267.js"
+  },
+  {
+    "revision": "9cbff665569acfe60ecdb1ec6c774ac1",
+    "url": "/JU_DISCUSSION/static/media/avatar.9cbff665.jpg"
+  },
+  {
+    "revision": "7e80fbea3b8bf2722a803ab3d39b3f71",
+    "url": "/JU_DISCUSSION/static/media/mountains.7e80fbea.jpg"
+  },
+  {
+    "revision": "fc952abe372740243a951ba308c711dd",
+    "url": "/JU_DISCUSSION/index.html"
+  }
+];
